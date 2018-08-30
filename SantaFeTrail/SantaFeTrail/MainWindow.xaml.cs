1e813aae3be16a31da1cceedf875dff5a2113e8a
Lance Govender <lg452323@gmail.com>
2018-08-31 01:00:06 +0200

    added movement stuctures, and trail map

--- SantaFeTrail/SantaFeTrail/MainWindow.xaml.cs
+++ SantaFeTrail/SantaFeTrail/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,55 @@ namespace SantaFeTrail
     /// </summary>
     public partial class MainWindow : Window
     {
+        char[][] map;
+        int width = 25;
+        int height = 25;
+
+        string[] chroma = new string[200]; // limited to that of 200 
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            //intialise the Game
+            initalise();
+
+        }
+
+        private void initalise()
+        {
+            //load the data into an arrays
+            
+            var filename = "c:\\Trail.Map";
+            string fullData = File.ReadAllText(filename);
+            map = new char[25][];
+            for (int i = 0; i < width; i++)
+            {
+                map[i] = new char[25];
+            }
+            int postionCount = 0;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    map[i][j] = fullData[postionCount];
+                    postionCount++;
+                }
+            }
+            tbLog.Text += fullData;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    tbLog.Text += map[i][j];
+                }
+                tbLog.Text += "\n";
+            }
+        }
+        
     }
 }
---
+++ SantaFeTrail/SantaFeTrail/MoveAnt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SantaFeTrail
+{
+    class MoveAnt
+    {
+        public int CalcScore(int[] MovementSet, char[][] map)
+        {
+            int width = 25;
+            int height = 25;
+            int score = 0;
+            int retJ = 0;
+            int retI = 0;
+            bool end = false;
+            // find A
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (map[i][j] == 'A')
+                    {
+                        retI = i;
+                        retJ = j;
+                        end = true;
+                        break;
+                    }
+                    retI = i;
+                }
+                if (end == true)
+                {
+                    break;
+                }
+            }
+            
+
+            for (int i = 0; i < MovementSet.Length; i++)
+            {
+                switch (MovementSet[i])
+                {
+                    case 0: // move up
+
+                        break;
+                    case 1: // move right
+
+                        break;
+                    case 2: // move down
+
+                        break;
+                    case 3: // move right
+
+                        break;
+                    default:
+
+                        break;
+
+                }
+
+            }
+            return score;
+        }
+
+        
+
+    }
+}
