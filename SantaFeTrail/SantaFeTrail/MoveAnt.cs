75c6fe92460ed1d50474510ebe31b22af74443b2
Lance Govender <lg452323@gmail.com>
2018-08-31 02:44:00 +0200

    Fixed dimensions

--- SantaFeTrail/SantaFeTrail/MoveAnt.cs
+++ SantaFeTrail/SantaFeTrail/MoveAnt.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace SantaFeTrail
 {
-    class MoveAnt
+    class MoveAnt // always clone before
     {
         public int CalcScore(int[] MovementSet, char[][] map)
         {
             int width = 25;
             int height = 25;
             int score = 0;
-            int retJ = 0;
-            int retI = 0;
+            int aPostiony = 0;
+            int apostionx = 0;
+            int currentx = 0;
+            int currenty = 0;
             bool end = false;
             // find A
             for (int i = 0; i < width; i++)
@@ -23,12 +25,11 @@ namespace SantaFeTrail
                 {
                     if (map[i][j] == 'A')
                     {
-                        retI = i;
-                        retJ = j;
+                        currentx = apostionx = i;
+                        currenty = aPostiony = j;
                         end = true;
                         break;
                     }
-                    retI = i;
                 }
                 if (end == true)
                 {
@@ -36,34 +37,58 @@ namespace SantaFeTrail
                 }
             }
             
-
             for (int i = 0; i < MovementSet.Length; i++)
             {
                 switch (MovementSet[i])
                 {
                     case 0: // move up
-
+                        if (currentx == width-1)
+                        {
+                            currentx = 0;
+                        }
+                        else
+                        {
+                            currentx -= 1;
+                        }
                         break;
                     case 1: // move right
-
+                        if (currenty == 24)
+                        {
+                            currenty = 0;
+                        }
+                        else
+                        {
+                            currenty += 1;
+                        }
                         break;
                     case 2: // move down
-
+                        if (currentx == 0)
+                        {
+                            currentx = 24;
+                        }
+                        else
+                        {
+                            currentx += 1;
+                        }
                         break;
-                    case 3: // move right
-
+                    case 3: // move left
+                        if (currenty == 0)
+                        {
+                            currenty = 24;
+                        }
+                        else
+                        {
+                            currenty -= 1;
+                        }
                         break;
-                    default:
-
-                        break;
-
                 }
-
+                if (map[currentx][currenty] == 1)
+                {
+                    map[currentx][currenty] = '0';
+                    score++;
+                }
             }
             return score;
         }
-
-        
-
     }
 }
